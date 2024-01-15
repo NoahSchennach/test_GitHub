3fe7f0b88c431e044aadab3f88698a5cc0ad3324
NoahSchennach <noah.schennach@hak-reutte.ac.at>
2024-01-15 10:24:57 +0100

    Local commit test end

--- src/testCode/Test.java
+++ src/testCode/Test.java
@@ -4,5 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello GITHUB");
 		System.out.println("Zeas");
+		System.out.println("Local commit test end");
 	}
 }
