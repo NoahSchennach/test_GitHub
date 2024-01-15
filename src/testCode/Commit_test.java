b72425d441c033fa7b2ca337bb20062a619f84fd
NoahSchennach <noah.schennach@hak-reutte.ac.at>
2024-01-15 10:16:55 +0100

    Commit Message Test

---
+++ src/testCode/Commit_test.java
@@ -0,0 +1,12 @@
+package testCode;
+
+public class Commit_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Commit Message test");
+		
+	}
+
+}
