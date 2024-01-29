666dc2be223e9fc074265c2717d3609269cf7d77
NoahSchennach <noah.schennach@hak-reutte.ac.at>
2024-01-29 11:35:55 +0100

    Created class Submission

--- src/socialnetwork/Submissions.java
+++ src/socialnetwork/Submissions.java
@@ -1,5 +1,13 @@
 package socialnetwork;
 
-public class Submissions {
+import java.util.ArrayList;
 
+public abstract class Submissions {
+	
+	private String userName;
+	private long timestamp;
+	private int likes;
+	private ArrayList<Comment> comment;
+
+	
 }
