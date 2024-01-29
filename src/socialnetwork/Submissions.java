420ce6325407d0f90a747fdde0b93054a1b49966
NoahSchennach <noah.schennach@hak-reutte.ac.at>
2024-01-29 11:45:33 +0100

    added getter and setter and the methods give like and and comment

--- src/socialnetwork/Submissions.java
+++ src/socialnetwork/Submissions.java
@@ -6,8 +6,36 @@ public abstract class Submissions {
 	
 	private String userName;
 	private long timestamp;
-	private int likes;
+	private int like;
 	private ArrayList<Comment> comment;
+	
+	public void giveLike() {
+		like = this.like + 1;
+	}
+	public abstract void giveComment();
+	
+	
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public long getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+	public int getLike() {
+		return like;
+	}
+	public void setLike(int likes) {
+		this.like = likes;
+	}
+	
+	
 
 	
 }
