e0f8d6012694c3da9d90d9bc571ce15e8234aca7
NoahSchennach <noah.schennach@hak-reutte.ac.at>
2024-02-26 09:48:12 +0100

    Added excercise2

--- src/array/Aufgabe2.java
+++ src/array/Aufgabe2.java
@@ -2,15 +2,25 @@ package array;
 
 public class Aufgabe2 {
 
-	public static boolean isProbablyApproaching(String[] signs) {
-		
-		
-			for(int i = 0; i<signs.length;i++) {
-				if(signs.length == 0) {
-					return false;
-				
-				}
-			}
-	}
-	}
+
+    public static boolean isProbablyApproaching(String[] signs) {
+        for(int i = 0; i < signs.length; i++) {
+            if(signs.length == 0 || signs[i].equals(null)) {
+                System.out.println("Fehler!");
+                return false;
+            }
+            else if(signs[i].equals(signs[i-1]) && signs[i].equals(signs[i-2]) && signs[i].equals(signs[i-3])){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+
+
+    }
+
 }
+
