76336ed200154f318555d9428d6e2d862546afa6
NoahSchennach <noah.schennach@hak-reutte.ac.at>
2024-03-04 11:44:47 +0100

    test

---
+++ src/arrayList/Excercise1.java
@@ -0,0 +1,35 @@
+package arrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Excercise1 {
+
+	
+	
+	public static void reduceToComments(List<String>lines) {
+		
+		for(int i = 0; i< lines.size()-1; i++) {
+			lin
+		}
+		
+		
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		List<String> lines = new ArrayList<String>();
+
+		lines.add("Magnetkopmass");
+		lines.add("Geschw. Wasserströmung");
+		lines.add("Wetter");
+		lines.add("Kommentar und allgemeine Beobachtung");
+		
+	System.out.println();
+	
+	}
+
+}
