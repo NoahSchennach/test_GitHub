04abf42b6d5a4f0badac38e7631bd697988402a5
NoahSchennach <noah.schennach@hak-reutte.ac.at>
2024-04-16 09:44:55 +0200

    potenz

---
+++ src/recrusion/PowerOf.java
@@ -0,0 +1,19 @@
+package recrusion;
+
+public class PowerOf {
+
+	
+	public static int powerOf(int base, int exponent) {
+		if(exponent == 0) {
+			return 1;
+		}
+		else {
+			return base * powerOf(base, exponent-1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println(powerOf(2,2));
+	}
+}
