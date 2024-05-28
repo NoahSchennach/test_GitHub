96973bdfce5eeefba53dfc9447c2e89cee6d0012
NoahSchennach <noah.schennach@hak-reutte.ac.at>
2024-05-28 12:43:42 +0200

    Beispiele Exceptions

---
+++ src/exceptions/Divide.java
@@ -0,0 +1,27 @@
+package exceptions;
+
+public class Divide {
+
+	public class DivisionHandler {
+
+		  public static void divide(int zähler, int nenner) {
+			  
+			  try {
+				int x = zähler;
+				int y= nenner;
+		    System.out.println(x/y); 
+		    } catch ( final ArithmeticException e) {
+		    	System.out.println("Division by zero is not allowed");
+		      
+		    }
+		  }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		    divide(10,2);
+	}
+
+	}
+}
+
--- src/exceptions/ToReadMusic.java
+++ src/exceptions/ToReadMusic.java
@@ -12,27 +12,45 @@ public class ToReadMusic {
 	private static final Pattern notes = Pattern.compile(
 			"C,?|D,?|E,?|F,?|G,?|A,?|B,?|c'?|d'?|e'?|f'?|g'?|a'?|b'?"
 			);
+	
 	public static void extractNotes() {
 		Scanner sc;
 		try {
 			sc = new Scanner(Paths.get("music.txt"));
-			
+			PrintWriter pw = new PrintWriter("file2.txt");
+			pw.write("M : C\n");
+			pw.write("L : 1/4\n");
+			pw.write("K : C\n \n");
 			while(sc.hasNextLine()) {
 				String nextNote = sc.nextLine();
 				if(!nextNote.isEmpty()) {
 					if(notes.matcher(nextNote).matches()) {
-					System.out.print(nextNote + " ");
+						pw.print(nextNote + " ");
+						
 					}
 					
 				}
 			}
 			sc.close();
+			pw.close();
 			
 		} catch (IOException e) {
 			System.out.println("Fehler");
 		}
 	}
+	
 	public static void main(String[] args) {
 		extractNotes();
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
